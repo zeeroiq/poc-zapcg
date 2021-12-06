e4001d6b2e33b9e2e6047555c1972d68ee848e97
Shri <shrikantvishwakarma@outlook.com>
2021-12-06 16:25:20 +0530

    initial code for db-excel-dmp service. closes #16

---
+++ db-excel-dump/src/main/java/com/zapcg/poc/DbExcelDumpApplication.java
@@ -0,0 +1,13 @@
+package com.zapcg.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExcelDumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbExcelDumpApplication.class, args);
+	}
+
+}
